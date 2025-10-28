44176338d19fc636d9e56dd691f2c7599da5017d
bac7220 <bacbac7220@gmail.com>
2025-10-28 13:41:28 +0900

    add:demojs作成

---
+++ demo.js
@@ -0,0 +1,9 @@
+  const simple = document.querySelector("#simple");
+      simple.addEventListener("click", function () {
+        simple.classList.toggle("is-action");
+      });
+      
+      const Rotation = document.querySelector("#Rotation");
+      Rotation.addEventListener("click", function () {
+        Rotation.classList.toggle("is-action");
+      });
